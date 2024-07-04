b02267b5fb8c1604103f55d46f0580d945499ad2
MarvelousMarvin7 <marvinagyei3@gmail.com>
2024-07-04 23:06:57 +0000

    [Feature:]Added Database storage and updated City and State class

--- models/state.py
+++ models/state.py
@@ -1,8 +1,28 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import Base, BaseModel
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+import models
+from models.city import City
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship(
+        'City',
+        cascade='all, delete, delete-orphan',
+        backref='state'
+    )
+
+    @property
+    def cities(self):
+        """returns the list of City instances with
+         state_id equals to the current State.id"""
+        city_list = []
+        for city in storage.all(City).values():
+            if city.state_id == self.id:
+                city_list.append(city)
+        return city_list
