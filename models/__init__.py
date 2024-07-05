86cc2ea632c73115e717776a6867236b0b1b0092
MarvelousMarvin7 <marvinagyei3@gmail.com>
2024-07-05 23:33:54 +0000

    Place, City, reviews and user updated including their relationships

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage and Database storage"""
+"""This module instantiates an object of class
+FileStorage and Database storage"""
+
 from models.engine.file_storage import FileStorage
 from models.engine.db_storage import DBStorage
 from os import getenv
