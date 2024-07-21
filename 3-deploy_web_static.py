0607afdfc29db360c1d4cb938a6187130be902b1
MarvelousMarvin7 <marvinagyei3@gmail.com>
2024-07-21 19:37:44 +0000

    edited

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -24,16 +24,14 @@ def do_pack():
 
 def do_deploy(archive_path):
     """deploy acrhived .tgz to webservers"""
-    if not os.path.exists(archive_path):
+    if (os.path.isfile(archive_path) is False):
         return False
 
     try:
         # Get the filename without extension
         filename = archive_path.split("/")[-1]
-        file_without_ext = filename.split('.')[0]
-
+        new_folder = ("/data/web_static/releases/" + filename.split('.')[0])
         put(archive_path, '/tmp/')
-        new_folder = ("/data/web_static/releases/ + {}".format(file_without_ext))
         run("sudo mkdir -p {}".format(new_folder))
         run("sudo tar -xzf /tmp/{} -C {}".format(filename, new_folder))
         run("sudo rm /tmp/{}".format(filename))
