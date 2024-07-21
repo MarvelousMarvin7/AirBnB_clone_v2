9aa09a9cd983fed7311b5c6cabdf20bad887d3ce
MarvelousMarvin7 <marvinagyei3@gmail.com>
2024-07-21 19:51:47 +0000

    edited

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -4,7 +4,7 @@
 """
 
 from fabric.api import *
-from fabric.operations import local, run, put, sudo
+from fabric.operations import env, run, put
 import os.path
 import time
 env.hosts = ['3.85.196.66', '54.175.225.232']
@@ -30,15 +30,15 @@ def do_deploy(archive_path):
     try:
         # Get the filename without extension
         filename = archive_path.split("/")[-1]
-        new_folder = ("/data/web_static/releases/" + filename.split('.')[0])
-        put(archive_path, '/tmp/')
-        run("sudo mkdir -p {}".format(new_folder))
-        run("sudo tar -xzf /tmp/{} -C {}".format(filename, new_folder))
-        run("sudo rm /tmp/{}".format(filename))
-        run("sudo mv {}/web_static/* {}/".format(new_folder, new_folder))
-        run("sudo rm -rf {}/web_static".format(new_folder))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s {} /data/web_static/current".format(new_folder))
+        new_folder = ("/data/web_static/releases/" + filename.split(".")[0])
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}".format(new_folder))
+        run("tar -xzf /tmp/{} -C {}".format(filename, new_folder))
+        run("rm /tmp/{}".format(filename))
+        run("mv {}/web_static/* {}/".format(new_folder, new_folder))
+        run("rm -rf {}/web_static".format(new_folder))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(new_folder))
         return True
     except:
         return False
